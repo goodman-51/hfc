a53f058b33f1c919390903c2bb91f163bd572fa2
Alex Goodman <agoodman@Alexs-MacBook-Pro-2.local>
2024-09-29 20:50:37 -0700

    feat(frontend): use new search from backend

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,5 +1,5 @@
 import api from "../lib/api";
 
-export const getUsers = (query = null) => api.get("/users", { query });
+export const getUsers = (query = null) => api.get("/users", { params: { query } });
 export const getUserContent = (userId) => api.get(`/content/${userId}`);
 export const setContentStatus = (postId, status) => api.post(`/content/${postId}/status`, { status });
