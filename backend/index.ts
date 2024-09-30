7b0a6ef0d4ef29fd30163d52ea9dbdf4f9dd10d6
Alex Goodman <agoodman@Alexs-MacBook-Pro-2.local>
2024-09-29 18:02:52 -0700

    feat(api): create endpoints to fetch content for a user and change the status of a piece of content

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize-typescript";
 import Content from "./models/Content.model";
+import { ContentStatus } from "./enums/ContentStatus.enum";
 import User from "./models/User.model";
 import express, { Request, Response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 4000;
@@ -23,6 +25,7 @@ app.use(
     credentials: true,
   })
 );
+app.use(bodyParser.json());
 
 // Get users route
 app.get("/users", async (req: Request, res) => {
@@ -48,8 +51,14 @@ app.get(
   ) => {
     const userId = req.params["userId"];
 
-    // TODO: Get content for user
-    res.json([]);
+    // Get content for user
+    const content = await Content.findAll({
+      where: {
+        userId
+      }
+    });
+
+    res.json(content);
   }
 );
 
@@ -57,6 +66,31 @@ app.get(
 // If the status is not valid, return 400 status code
 // If the content does not exist, return 404 status code
 // If the content is already approved, you can't change the status, return 400 status code
+app.post("/content/:postId/status",
+  async (
+    req: Request<{
+      postId: number;
+      status: ContentStatus;
+    }>,
+    res: Response<{
+      status: ContentStatus;
+    }>
+  ) => {
+    const postId = req.params["postId"];
+    const status = req.body["status"];
+
+    if (!Object.values(ContentStatus).includes(status)) {
+      res.sendStatus(400);
+    }
+
+    const [ rowsUpdated ] = await Content.update({ status }, { where: { id: postId }});
+
+    if (rowsUpdated === 0) res.sendStatus(404);
+
+    res.json(status);
+  }
+)
+
 
 // Search endpoint
 // Search by user title, user tags and content title
